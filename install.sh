fb8738b3bc04a756278424d3dc03ca6cf0490714
David Powell <david.powell@monash.edu>
2014-07-07 12:23:09 +1000

    Install script to copy over zip-output.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cabal exec make PRODUCTION=1
 
 sudo -u prokka  mkdir -p ~prokka/bin ~prokka/user-files/uploads ~prokka/user-files/status ~prokka/user-files/output
 sudo -u prokka cp -f prokka-web prokka-runner ~prokka/bin
+sudo -u prokka cp -f zip-output.sh ~prokka
 sudo -u prokka cp -r static ~prokka
 sudo -u prokka cp -r templates ~prokka
 
